cde97e2d142b427384d5b4d77001edaff4668f9f
kernel1994 <971785306@qq.com>
2016-04-18 23:09:27 +0800

    修复 SQL-update 语句的 bug

--- asset/js/app.js
+++ asset/js/app.js
@@ -10,3 +10,25 @@ var isEmptyString = function (str) {
     if (str2 === undefined)
         return true;
 };
+
+// 根本不需要删除 vmData.checkedField 和 vmData.checkedCondition 中的元素
+// 因为每次点击 btn 都会将两个数组置空，再计算填充元素
+// 所以这个方法废弃，权当学习删除数组中特定元素例子
+// var deleteEle = function (arr, eleName) {
+//     arr.forEach(function (e, i, a) {
+//         if (e.name === eleName) {
+//             return arr.splice(i, 1);
+//         }
+//     });
+// };
+
+/**
+* 将所有checkbox 呈未选中状态
+*/
+var unCheckAll = function () {
+    $("input[type=checkbox]").each(function () {
+        if (this.checked) {
+            this.checked = false;
+        }
+    });
+}
